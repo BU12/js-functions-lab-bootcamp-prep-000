b6c3b320e176bffb428bba3617ac9dba1ae50e69
BU12 <williampeterson23@gmail.com>
2017-09-15 03:34:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // write your code below!
 function happyHolidays() {
-  return 'Happy holidays!'
+  return "Happy holidays!"
 }
 
 function happyHolidaysTo(name) {
+  var name = "${name}"
   return 'Happy holidays, '${name}'!'
 }
 
